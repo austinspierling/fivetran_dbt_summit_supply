52e280889f36764b0148c3c92c239446dc1a4359
Austin Spierling <austin.spierling@gmail.com>
2025-09-20 11:47:47 -0600

    Revise stg_salesforce__contact SQL view definition
    Updated SQL view to select additional fields and filter deleted records.

--- models/staging/stg_salesforce__contact.sql
+++ models/staging/stg_salesforce__contact.sql
@@ -1,9 +1,12 @@
-{{ config(materialized='view') }}
+-- models/staging/stg_salesforce__contact.sql
 
-select
-    id as contact_id,
-    first_name,
-    last_name,
-    email,
-    current_timestamp as _loaded_at
-from {{ source('salesforce', 'contact') }}
+SELECT id                AS contact_id,
+       name              AS full_name,
+       email,
+       mailingstreet     AS mailing_street,
+       mailingcity       AS mailing_city,
+       mailingstate      AS mailing_state,
+       mailingpostalcode AS mailing_postal_code,
+       loyalty_tier__c   AS loyalty_tier
+FROM {{ source('rma_salesforce', 'contact') }}
+WHERE is_deleted = FALSE
