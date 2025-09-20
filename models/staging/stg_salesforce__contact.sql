807bd5d88da6b07a44f29e2ad71b3245209807e5
Austin Spierling <austin.spierling@gmail.com>
2025-09-20 12:17:58 -0600

    Add account_id and name fields to contact query

--- models/staging/stg_salesforce__contact.sql
+++ models/staging/stg_salesforce__contact.sql
@@ -1,12 +1,13 @@
 -- models/staging/stg_salesforce__contact.sql
 
-SELECT id                AS contact_id,
-       name              AS full_name,
+SELECT account_id,
+       first_name,
+       last_name,
        email,
-       mailingstreet     AS mailing_street,
-       mailingcity       AS mailing_city,
-       mailingstate      AS mailing_state,
-       mailingpostalcode AS mailing_postal_code,
-       loyalty_tier__c   AS loyalty_tier
+       mailing_street,
+       mailing_city,
+       mailing_state,
+       mailing_postal_code,
+       loyalty_tier__c
 FROM {{ source('rma_salesforce', 'CONTACT') }}
 WHERE is_deleted = FALSE
